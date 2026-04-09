0ea03c453924668f8be009d607fc7c059f550eda
ZIMAX <ZIMAX@orenoMacBook-Pro.local>
2026-04-09 22:26:00 +0900

    fix: ロゴを中央寄せに修正

--- utils.py
+++ utils.py
@@ -68,7 +68,9 @@ def display_logo(show_line=True):
     if os.path.exists("logo.png"):
         try:
             img = Image.open("logo.png")
-            st.image(img, use_container_width=True)
+            col1, col2, col3 = st.columns([1, 2, 1])
+            with col2:
+                st.image(img, use_container_width=True)
         except:
             st.markdown("<h1 style='text-align: center;'>🦝 TASUKARU</h1>", unsafe_allow_html=True)
     else:
